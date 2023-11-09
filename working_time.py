efed3cc0235f887f1905bdf6028508f8e66775ff
Resistor-git <partizan-resistor@yandex.ru>
2023-11-09 17:09:14 +0300

    Add a template function to break working periods into intervals

--- working_time.py
+++ working_time.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta
-from typing import List, Dict
+from typing import List, Dict, Tuple, Optional
 
 busy = [
     {'start' : '10:30',
@@ -21,6 +21,7 @@ busy = [
 
 work_begins: str = '09:00'
 work_ends: str = '21:00'
+default_interval: int = 30
 
 
 # tdelta = timedelta(minutes=30)
@@ -28,8 +29,8 @@ start: datetime = datetime.strptime(work_begins, '%H:%M')
 stop: datetime = datetime.strptime(work_ends, '%H:%M')
 
 
-def working_time(work_start: datetime, work_end: datetime, breaks: List[Dict[str, str]]) -> None:
-    working_periods: List = []
+def working_time(work_start: datetime, work_end: datetime, breaks: List[Dict[str, str]]) -> List[Tuple[datetime, datetime]]:
+    working_periods: List[Optional[Tuple[datetime, datetime]]] = []  # pretty sure the type hint is wrong...
     break_periods: List = []
     previous_stop: datetime = start
 
@@ -47,12 +48,17 @@ def working_time(work_start: datetime, work_end: datetime, breaks: List[Dict[str
     
     if previous_stop < work_end:
         working_periods.append((previous_stop, work_end))
-        
 
-    for period in working_periods:
-        start_time = period[0].strftime("%H:%M")
-        stop_time = period[1].strftime("%H:%M")
-        print(f'Working period: {start_time} - {stop_time}')
+    # for period in working_periods:
+    #     start_time = period[0].strftime("%H:%M")
+    #     stop_time = period[1].strftime("%H:%M")
+    #     print(f'Working period: {start_time} - {stop_time}')
+    return working_periods
+
+
+def break_into_intervals(periods: List[Tuple[datetime, datetime]], interval) -> List[Tuple[datetime, datetime]]:
+    interval = timedelta(minutes=interval)
+    ...
 
 
 working_time(start, stop, busy)
